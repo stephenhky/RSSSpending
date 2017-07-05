3c16b75f8c7c4f62f94b9d980808b0650471bc8f
stephenhky <stephenhky@yahoo.com.hk>
2017-07-05 15:45:34 -0400

    accessed and read contents of workbook

--- draft/ReadGGSData.R
+++ draft/ReadGGSData.R
@@ -1,5 +1,19 @@
 
+# loading libraries
 library(googlesheets)
 library(dplyr)
 
+# list
 spreadsheets<- gs_ls("SS Expenditure*")
+
+# identify file
+filename_search<- 'SS Expenditure (2017) test'
+spreadsheet<- spreadsheets %>% filter(sheet_title==filename_search)
+
+# register
+ssspend_wb<- gs_title(filename_search)
+# to browse: ssspend_wb %>% gs_browse()
+sheets<- gs_ws_ls(ssspend_wb)
+# to read sheet: ssspend_wb %>% gs_read(ws="<name>")
+
+# edit cells
