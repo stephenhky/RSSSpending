128ecc76bf231c89656e3434b8c12a6d0cf31e32
stephenhky <stephenhky@yahoo.com.hk>
2017-07-25 11:29:28 -0400

    more NLP routines established and checked

--- draft/ReadGGSData.R
+++ draft/ReadGGSData.R
@@ -40,8 +40,6 @@ get.spend.data.months<- function(wb, months) {
 
 # natural language processing
 # ref: https://stackoverflow.com/questions/30995232/how-to-use-opennlp-to-get-pos-tags-in-r
-xwalk<- read.csv('crosswalk.csv', stringsAsFactors = FALSE, header=FALSE)
-xwalk.map<- xwalk$V2;    names(xwalk.map)<- xwalk$V1;   rm(xwalk)
 word_token_annotator<- Maxent_Word_Token_Annotator()
 postag_annotator<- Maxent_POS_Tag_Annotator()
 tokenize.int<- function(string) {
@@ -49,14 +47,58 @@ tokenize.int<- function(string) {
   annobj<- Annotation(1L, 'sentence', 1L, nchar(s))
   annotate(s, word_token_annotator, annobj)
 }
+tokenize<- function(string) {
+  tokentbl<- tokenize.int(string)
+  mapply(function(start, end) substr(string, start, end), 
+         tokentbl$start[ tokentbl$type=='word'], 
+         tokentbl$end[ tokentbl$type=='word'])
+}
 tag.POS<- function(string) {
-  annotate(as.String(string), postag_annotator, tokenize.int(as.String(string)))
+  tbl<- annotate(as.String(string), postag_annotator, tokenize.int(as.String(string)))
+  unlist(lapply(tbl$features, '[[', 'POS'))
 }
 stem.all.tokens<- function(string) {
-  
+  tokentbl<- tokenize.int(as.String(string))
+  mapply(function(start, end) wordStem(substr(string, start, end)), 
+         tokentbl$start[ tokentbl$type=='word'], 
+         tokentbl$end[ tokentbl$type=='word'])
 }
-normalize.category<- function(category) {
+
+# category normalization
+xwalk<- read.csv('crosswalk.csv', stringsAsFactors = FALSE, header=FALSE)
+xwalk.map<- as.list(xwalk$V2);    names(xwalk.map)<- xwalk$V1;    rm(xwalk)
+categories.tohash<- function(categories) {
+  hash<- list()
+  for (category in categories) {
+    stemmed.category<- tolower(stem.all.tokens(category))
+    hash[[stemmed.category]]<- append(hash[[stemmed.category]], category)
+  }
+  hash
+}
+score.word<- function(word) {
+  poss<- tag.POS(word)
+  capitalized<- Reduce(function(a, b) a & b, mapply(function(token) grepl('^[A-Z]', token), tokenize(word)))
   
+  # scoring
+  score<- 0
+  # Rule 1: prefer '-ing' ending
+  if ('VBG' %in% poss) score<- score+1
+  # Rule 2: prefer capitalized start
+  if (capitalized) score<- score+1
+  # Rule 3: prefer singular
+  if (('NN' %in% poss) | ('NNP' %in% poss)) score<- score+1
+  
+  score
+}
+choose.bestword<- function(words) names(which.max(mapply(score.word, words)))
+normalize.category<- function(category, all.hash, xwalk.map) {
+  stemmed.category<- tolower(stem.all.tokens(category))
+  if (stemmed.category %in% names(xwalk.map)) stemmed.category<- xwalk.map[[stemmed.category]]
+  if (stemmed.category %in% names(all.hash)) {
+    choose.bestword(all.hash[[stemmed.category]])
+  } else {
+    category
+  }
 }
 
 # edit cells
